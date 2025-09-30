3de0efe986b13cca6cd258828a60d33d35ed6b72
Daniel Kaiser <dankaiser1808@gmail.com>
2025-09-29 20:58:41 -0500

    make bootstrap.sh executable

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ if [ -z "$AWS_ACCESS_KEY" ] || [ -z "$AWS_SECRET_ACCESS_KEY" ] || [ -z "$CLUSTER
   exit 1
 fi
 
-kubectl create namespace external-secrets
+#kubectl create namespace external-secrets
 
 cat <<EOF | kubectl apply -f -
 apiVersion: v1
@@ -19,9 +19,9 @@ stringData:
     secret-access-key: ${AWS_SECRET_ACCESS_KEY}
 EOF
 
-helm repo add cilium https://helm.cilium.io
-helm repo update
-helm install cilium cilium/cilium --version 1.18.2 -n kube-system -f https://raw.githubusercontent.com/dankaiser1808/homelab2.0/refs/heads/main/core/controllers/base/cilium/values.yaml
+#helm repo add cilium https://helm.cilium.io
+#helm repo update
+#helm install cilium cilium/cilium --version 1.18.2 -n kube-system -f https://raw.githubusercontent.com/dankaiser1808/homelab2.0/refs/heads/main/core/controllers/base/cilium/values.yaml
 
 flux bootstrap github \
   --owner=dankaiser1808 \
